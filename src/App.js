3b5c2bd78181ca9e29f3dccae216965161ba8606
semanurbilada <semanurbilada@hotmail.com>
2023-02-12 20:24:42 +0300

    react router fixed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./css/index.css";
 import Layout from "./core/Layout.js";
 import Default from "./pages/Default.js";
