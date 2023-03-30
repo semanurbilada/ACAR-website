f1e56e20359fcb458ecadcda02bcb5425a48f97c
semanurbilada <semanurbilada@hotmail.com>
2023-03-30 09:03:47 +0300

    FixedButons designed, needs to be configured

---
+++ src/components/FixedButtons.js
@@ -0,0 +1,12 @@
+export default function FixedButtons() {
+    return(
+        <div 
+            className="fixed md:bottom-10 bottom-40 md:right-10 right-5 flex md:flex-row flex-col items-center md:gap-6 gap-4 z-30 md:text-xl text-base"
+        >
+            <button className="fixed-button">TR</button>
+            <button className="fixed-button">EN</button>
+        </div>
+    );
+
+    //TO DO: butonlar, useContext'te tercihe göre render olmalı !
+}
--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import FixedButtons from "../components/FixedButtons";
 
 export default function Layout({ pageContent }) {
   return (
     <div>
       <Navbar />
       {pageContent}
+      <FixedButtons />
       <Footer />
     </div>
   );
