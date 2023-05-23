a141a97f822a4d57c34600f2e482bd1f34dee99a
semanurbilada <semanurbilada@hotmail.com>
2023-05-23 19:59:55 +0300

    Scroll to top function added

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,14 +1,25 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import FixedButtons from "../components/FixedButtons";
 
 export default function Layout({ pageContent }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+    });
+  }, [location]);
+
   return (
-    <div>
+    <section>
       <Navbar />
       {pageContent}
       <FixedButtons />
       <Footer />
-    </div>
+    </section>
   );
 }
