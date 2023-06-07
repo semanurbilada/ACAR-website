9f0e8aba4aa12b7e2803179113721184f94bdfcd
semanurbilada <semanurbilada@hotmail.com>
2023-06-07 19:30:27 +0300

    useContext added, needs to be configured

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, createContext } from "react";
 import {
   Route, 
   Routes,
@@ -14,18 +14,28 @@ import Products from "./pages/Products";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
 
+export const StateContext = createContext({
+  language: null,
+});
+
 export default function App() {
+  const [ language, setLanguage ] = useState("TR");
+
   return (
     <div className="bg-gray-300">
       <Router>
-        <Routes>
-          <Route path="/" element={<Language />} />
-          <Route path="/home" element={<Layout pageContent={<Home />} />} />
-          <Route path="/places" element={<Layout pageContent={<Places />} />} />
-          <Route path="/products" element={<Layout pageContent={<Products />} />} />
-          <Route path="/about" element={<Layout pageContent={<About />} />} />
-          <Route path="/contact" element={<Layout pageContent={<Contact />} />} />
-        </Routes>
+        <StateContext.Provider 
+          value={{ language, setLanguage }}
+        >
+          <Routes>
+            <Route path="/" element={<Language />} />
+            <Route path="/home" element={<Layout pageContent={<Home />} />} />
+            <Route path="/places" element={<Layout pageContent={<Places />} />} />
+            <Route path="/products" element={<Layout pageContent={<Products />} />} />
+            <Route path="/about" element={<Layout pageContent={<About />} />} />
+            <Route path="/contact" element={<Layout pageContent={<Contact />} />} />
+          </Routes>
+        </StateContext.Provider>
       </Router>
     </div>
   );
---
+++ src/components/services/data.js
@@ -0,0 +1,119 @@
+export const navlinks = {
+    home: {
+        TR: "Ana Sayfa",
+        EN: "Home"
+    },
+    places: {
+        TR: "Depolarımız",
+        EN: "Places"
+    },
+    products: {
+        TR: "Ürünlerimiz",
+        EN: "Products"
+    },
+    about: {
+        TR: "Hakkımızda",
+        EN: "About"
+    },
+    contact: {
+        TR: "İletişim",
+        EN: "Contact"
+    }
+};
+
+export const HomeSwiperData = {
+    TR: [
+        { content: "Demir, Çelik Sektörünün en köklülerinden!" },
+        { content: "Kaliteli Demir, Çelik ve Metal için doğru adres!" },
+        { content: "Sektörün lideri olarak Demir, Çelik ve Metal Geri Dönüşümüne katkıda bulunuyoruz!" }
+    ],
+    EN: [
+        { content: "The most rooted in the Steel Industry!" },
+        { content: "The right address for Quality Iron, Steel and Metal!" },
+        { content: "As the leader of the industry, we contribute to Iron, Steel and Metal Recycling!" }
+    ]
+};
+
+//Places + services data;
+export const InfoSectionData = {
+};
+
+//customize edilmeli!
+export const FooterData = {
+    TR: [
+        {
+            title: null,
+            content: "40 Yılı aşkın tecrübemizle! Demir, Çelik ve Metal ürünleri ticareti yaparak sektöre katkıda bulunuyoruz.",
+            link: "Devamını Oku"
+        },
+        {
+            title: "İletişim",
+            content: "Telefon: (0262) 658 13 60 Mail: info@acargeridonusum.com.tr",
+            link: "Sosyal Medyadan bizi takip edin:"
+        },
+        {
+            title: null,
+            content: "Adresimize ulaşmak için:",
+            link: null
+        }
+    ],
+    EN: [
+        {
+            title: null,
+            content: "With over 40 years of experience! We contribute to the sector by trading Iron, Steel and Metal products.",
+            link: "Read More"
+        },
+        {
+            title: "Contact",
+            content: "Phone: (0262) 658 13 60 Mail: info@acargeridonusum.com.tr",
+            link: "Follow us in Social Media:"
+        },
+        {
+            title: null,
+            content: "To reach our address:",
+            link: null
+        }
+    ],
+};
+
+//ComingSoon component;
+export const ProductsData = {
+    TR: [
+        "Detaylı ürünler sayfamız yakında sizlerle! Ürün stoklarımız hakkında bilgi almak için bizimle iletişime geçin:"
+    ],
+    EN: [
+        "Our detailed products page will be here soon! Contact us for information about our product stocks:"
+    ]
+};
+
+export const AboutUsData = {
+    TR: [
+        {
+            title: "40 Yılı aşkın tecrübemizle!",
+            maincontent: "Demir, Çelik ve Metal ürünleri ticareti yaparak sektöre katkıda bulunuyoruz. 1965 yılında babalarımızın başlatmış olduğu bu alanda, 2014 yılından itibaren Acar Geri Dönüşüm olarak devam ediyoruz. Sektöre katkıda bulunmak açısından; bu zamana kadar edindiğimiz, babadan oğula geçen tecrübelerimizi kullanıyoruz.",
+            secondcontent: "Demir, çelik, metal, sac ve profil gibi ürünleri farklı birçok sanayi kolundan temin edip yurt içi ve yurt dışı ticaretini yapıyoruz. Ticaretini yaptığımız ürünlerimiz çoğunlukla ağır sanayi mamulleri olup, 1 ve 2.kalite malzeme tedariği yapmaktayız. Hem ihracat, hem de ithalat yaparak firma çizgimizi bozmadan yolumuzda ilerlemeye çalışıyoruz."
+        },
+    ],
+    EN: [
+        {
+            title: null,
+            maincontent: "We contribute to the sector by trading Iron, Steel and Metal products. We have been continuing in this field, which was started by our fathers in 1965, as Acar Geri Dönüşüm since 2014. In terms of contributing to the sector; We use the experiences we have gained so far, passed down from father to son.",
+            secondcontent: "We procure products such as iron, steel, metal, sheet metal and profile from many different industries with national and international trade. The products we trade are mostly heavy industry products, and we supply 1st and 2nd quality materials. We are trying to move forward on our way without breaking our company line by both exporting and importing."
+        }
+    ]
+}
+
+export const ContactData = {
+    TR: [
+        {
+            click: "Click for WhatsApp!",
+            phone: "Office: (0262) 658 13 60"
+        }
+    ],
+    EN: [
+        {
+            click: "Click for E-Mail!",
+            phone: null
+        }
+    ]
+}
